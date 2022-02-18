22b80be3e98b06b986dde1b3c592486eebafbe77
sabnavneev <sabnavneev@gmail.com>
2022-02-18 09:54:26 +0100

    Huiswerk af

---
+++ src/nl/novi/backend/java/hello/Main.java
@@ -0,0 +1,30 @@
+package nl.novi.backend.java.hello;
+
+//Het programma moet een main Klasse
+public class Main {
+
+    //Het programma moet een main methode hebben
+    public static void main(String[] args) {
+
+        //Het programma moet een variabele bevatten
+        String name = "Bas";
+        int age = 38;
+        String cityOfBirth = "Utrecht";
+
+        // Het programma moet jouw een welkomsgroet geven
+        System.out.println("Hallo " + name + ", geboren in " + cityOfBirth + " en inmiddels al weer " + age + " jaar oud.");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
